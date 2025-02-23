922aea8917abcc21650261d0d79f33196d5f714b
Adedayo Olatunde <olatundeadedayo1@gmail.com>
2025-02-23 13:28:07 +0100

    changed port

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(5000);
+  await app.listen(3000);
   logger.log('server running on port 3000');
 }
 
